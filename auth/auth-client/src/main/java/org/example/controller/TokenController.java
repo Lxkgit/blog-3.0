f6c143a593481ddafdfa03c3173a8434de7a3c59
lixinkan <lixinkan@kangxukeji.com>
2025-02-18 18:31:52 +0800

    oauth模板修改

--- auth/auth-client/src/main/java/org/example/controller/TokenController.java
+++ auth/auth-client/src/main/java/org/example/controller/TokenController.java
@@ -2,6 +2,7 @@ package org.example.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequestMapping("/")
 public class TokenController {
 
     @GetMapping("/token")

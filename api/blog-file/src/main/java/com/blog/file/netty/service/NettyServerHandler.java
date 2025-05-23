b8e5f6cc078e2aa96857d1efaa8ed643866477b1
lixinkan <lixinkan@kangxukeji.com>
2025-05-23 18:28:54 +0800

    【功能修改】: log4j2日志输出功能修改

--- api/blog-file/src/main/java/com/blog/file/netty/service/NettyServerHandler.java
+++ api/blog-file/src/main/java/com/blog/file/netty/service/NettyServerHandler.java
@@ -3,6 +3,7 @@ package com.blog.file.netty.service;
 
 import com.alibaba.fastjson2.JSONObject;
 import com.alibaba.fastjson2.TypeReference;
+import com.blog.core.utils.RSAUtil;
 import com.blog.file.netty.domain.dto.NettyClientChannel;
 import com.blog.file.netty.domain.dto.NettyPacket;
 import com.blog.file.mapper.DeviceMapper;
@@ -17,6 +18,8 @@ import io.netty.handler.timeout.IdleStateEvent;
 import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
@@ -29,11 +32,13 @@ import java.util.concurrent.ConcurrentHashMap;
  * @Author: lxk
  * @date 2024/1/6 15:16
  */
-@Slf4j
+
 @Component
 @RequiredArgsConstructor
 @ChannelHandler.Sharable
 public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(NettyServerHandler.class);
 
     // 全局map，保存通道编码与客户端编码 （用户服务端监听到客户端断开连接，移除map通道）
     public static final Map<ChannelId, ChannelHandlerContext> channelMap = new ConcurrentHashMap<>();
@@ -58,11 +63,11 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
         ChannelId channelId = ctx.channel().id();
         // 如果map中不包含此连接，就保存连接
         if (channelMap.containsKey(channelId)) {
-            log.info("客户端【{}】是连接状态，连接通道数量:{}", channelId, channelMap.size());
+            logger.info("客户端【{}】是连接状态，连接通道数量:{}", channelId, channelMap.size());
         } else {
             // 保存连接
             channelMap.put(channelId, ctx);
-            log.info("netty client【{}】 connected [clientIp:{} clientPort:{}]", channelId, clientIp, clientPort);
+            logger.info("netty client【{}】 connected [clientIp:{} clientPort:{}]", channelId, clientIp, clientPort);
         }
     }
 
@@ -80,7 +85,7 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
         if (channelMap.containsKey(channelId)) {
             // 删除连接
             removeChannelByChannelId(channelId);
-            log.warn("客户端【{}】断开Netty连接!![clientIp:{} clientPort:{}]", channelId, clientIp, clientPort);
+            logger.warn("客户端【{}】断开Netty连接!![clientIp:{} clientPort:{}]", channelId, clientIp, clientPort);
         }
     }
 
@@ -96,8 +101,7 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
             // 发布自定义Netty数据包处理事件
             applicationEventPublisher.publishEvent(new NettyPacketEvent(ctx.channel().id(), nettyPacket));
         } catch (Exception e) {
-            NettyPacket<String> nettyResponse = NettyPacket.buildRequest("报文解析失败!!");
-            ctx.writeAndFlush(JSONObject.toJSONString(nettyResponse));
+            logger.error("netty msg:{} 数据解析异常:{}", msg.toString(), e.getMessage(), e);
         }
     }
 
@@ -107,13 +111,13 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
         if (evt instanceof IdleStateEvent) {
             IdleStateEvent event = (IdleStateEvent) evt;
             if (event.state() == IdleState.READER_IDLE) {
-                log.warn("Client: 【{}】 READER_IDLE 读超时", socketString);
+                logger.warn("Client: 【{}】 READER_IDLE 读超时", socketString);
                 ctx.close();
             } else if (event.state() == IdleState.WRITER_IDLE) {
-                log.warn("Client: 【{}】 WRITER_IDLE 写超时", socketString);
+                logger.warn("Client: 【{}】 WRITER_IDLE 写超时", socketString);
                 ctx.close();
             } else if (event.state() == IdleState.ALL_IDLE) {
-                log.warn("Client: 【{}】 ALL_IDLE 读/写超时", socketString);
+                logger.warn("Client: 【{}】 ALL_IDLE 读/写超时", socketString);
                 ctx.close();
             }
         }

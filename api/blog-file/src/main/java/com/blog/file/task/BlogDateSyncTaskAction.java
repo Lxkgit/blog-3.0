6281d327b7b8be7edba71c96805ba9fcb4fea2f1
lxk <470687917@qq.com>
2026-07-14 17:11:44 +0800

    【功能修改】: 服务器数据备份任务bug修复-2

--- api/blog-file/src/main/java/com/blog/file/task/BlogDateSyncTaskAction.java
+++ api/blog-file/src/main/java/com/blog/file/task/BlogDateSyncTaskAction.java
@@ -46,7 +46,8 @@ public class BlogDateSyncTaskAction extends SystemTimerAction {
         String blogFilePath = Constant.FTP_PATH_SYSTEM_TEMP + "/" + MyStringUtils.getRandomString(6);
         taskMsgHead.setTaskParam(context.get("param"));
         String filePath = nettySyncFileService.syncBlogDataFirstStep(blogFilePath, MsgHead.buildTaskMsgHead(taskMsgHead.getUserId(), taskMsgHead));
-        JSONObject jsonObject = JSONObject.parseObject(filePath);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("tempFilePath", filePath);
         return jsonObject.toJSONString();
     }
 

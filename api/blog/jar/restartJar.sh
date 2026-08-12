e096f0a03dbb7f1a1f6c653f61c835f23a675f84
lxk <470687917@qq.com>
2026-08-12 14:09:22 +0800

    【功能修改】: 更新jar包打包配置-6

--- api/blog/jar/restartJar.sh
+++ api/blog/jar/restartJar.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
 # 启动 auth 服务
-/opt/docker/jar/auth/restart.sh
+/opt/docker/files/jar/auth/restart.sh
 sleep 1m
 
 # 启动 gateway 服务
-/opt/docker/jar/gateway/restart.sh
+/opt/docker/files/jar/auth/restart.sh
 sleep 1m
 
 # 启动 content 服务
-/opt/docker/jar/content/restart.sh
+/opt/docker/files/jar/auth/restart.sh
 sleep 1m
 
 # 启动 file 服务
-/opt/docker/jar/file/restart.sh
+/opt/docker/files/jar/auth/restart.sh

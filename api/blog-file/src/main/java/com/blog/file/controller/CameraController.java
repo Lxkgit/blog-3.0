1a9c9de7ec6df752d6b86aea3283c1d57ded2b77
lxk <470687917@qq.com>
2026-08-28 10:55:28 +0800

    【功能修改】: 修改视频流授权配置-13

--- api/blog-file/src/main/java/com/blog/file/controller/CameraController.java
+++ api/blog-file/src/main/java/com/blog/file/controller/CameraController.java
@@ -44,7 +44,7 @@ public class CameraController {
      * @param token Token
      */
     @GetMapping("/token/validate")
-    public ResponseEntity<Void> validateToken(@RequestHeader(value = "X-Video-Token", required = false) String token) {
+    public ResponseEntity<Void> validateToken(@RequestParam("stream") String token) {
         JSONObject tokenInfo = cameraService.validateToken(token);
         if (tokenInfo == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();

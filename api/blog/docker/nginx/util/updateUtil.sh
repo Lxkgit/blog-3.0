82a00cddd02f246e19f3dd3346a18a27ae6c92fa
lxk <470687917@qq.com>
2026-09-03 16:02:23 +0800

    【功能修改】: 新增game模块打包

--- api/blog/docker/nginx/util/updateUtil.sh
+++ api/blog/docker/nginx/util/updateUtil.sh
@@ -9,12 +9,19 @@ mkdir -p /opt/docker/nginx/util/logs
 /opt/docker/ci/shell/updateCode.sh -n blog
 cp /opt/docker/ci/code/blog-3.0/api/blog/docker/nginx/util/conf/nginx.conf /opt/docker/nginx/util/conf/
 
-# 更新并打包 web 服务
+# 更新并打包 excel 服务
 /opt/docker/ci/shell/buildController.sh -e pro -p excel
 
 rm -rf /opt/docker/nginx/util/html
 mkdir -p /opt/docker/nginx/util/html/excel
 mv /opt/docker/ci/code/web-excel/dist/* /opt/docker/nginx/util/html/excel
+
+# 更新并打包 game 服务
+/opt/docker/ci/shell/buildController.sh -e pro -p game
+
+rm -rf /opt/docker/nginx/util/game
+mkdir -p /opt/docker/nginx/util/html/game
+mv /opt/docker/ci/code/game-library/dist/* /opt/docker/nginx/util/html/game
 
 # 重启 Nginx Util 服务
 /opt/docker/nginx/util/restartUtil.sh

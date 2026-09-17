024eb5b6721f4666419d3f4760ec949ac78a45e0
lxk <470687917@qq.com>
2026-09-17 14:36:01 +0800

    【功能修改】: 服务配置更新-4

--- api/blog/docker/nginx/web/updateWeb.sh
+++ api/blog/docker/nginx/web/updateWeb.sh
@@ -15,6 +15,10 @@ mv /opt/docker/ci/code/blog-3.0/web/dist/* /opt/docker/nginx/web/html
 # 更新nginx服务配置
 cp /opt/docker/ci/code/blog-3.0/api/blog/docker/nginx/web/conf/nginx.conf /opt/docker/nginx/web/conf/
 
+# 更新服务重启脚本
+cp /opt/docker/ci/code/blog-3.0/api/blog/docker/nginx/web/restartWeb.sh /opt/docker/nginx/web/
+chmod +x /opt/docker/nginx/web/restartWeb.sh
+
 # 重启 web 服务
 /opt/docker/nginx/web/restartWeb.sh
 
